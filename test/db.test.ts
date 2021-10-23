7783faad4a6af28611abbf8bd237449ca5b38483
Zhang Junyu <junyu92@gmail.com>
2021-10-23 03:52:07 +0000

    add testcase for in-memory merkletree

--- test/db.test.ts
+++ test/db.test.ts
@@ -3,7 +3,7 @@ import { MerkleTree } from "../src/merkle-tree-large";
 
 var assert = require('assert');
 
-async function main() {
+async function testDBMerkleTree() {
     const merkle_tree = new MerkleTree();
 
     await merkle_tree.loadSnapshot("0");
@@ -53,6 +53,34 @@ async function main() {
     });
 
     await merkle_tree.closeDb();
+}
+
+async function testInMemoryMerkleTree() {
+    const merkle_tree = new MerkleTree(true);
+
+    await merkle_tree.getNode("0001").then((node) => {
+        assert.ok(node === undefined)
+    });
+
+    await merkle_tree.setNode("0001", new Field(1));
+    await merkle_tree.getNode("0001").then((node) => {
+        assert.ok(node!.v.eq(new Field(1).v))
+    });
+
+    // lastestSnapshot should always return string "0"
+    await merkle_tree.lastestSnapshot().then((node) => {
+        assert.equal(node, "0");
+    });
+
+    // should do nothing
+    await merkle_tree.loadSnapshot("0");
+    await merkle_tree.endSnapshot();
+    await merkle_tree.closeDb();
+}
+
+async function main() {
+    await testDBMerkleTree();
+    await testInMemoryMerkleTree();
 }
 
 main().then(() => console.log("done"))
