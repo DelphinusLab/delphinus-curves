33750095b6e0798cfde1f8735a306d56f3551129
Zhang Junyu <junyu92@gmail.com>
2021-10-23 03:52:07 +0000

    add testcase for in-memory merkletree

--- test/db.test.ts
+++ test/db.test.ts
@@ -3,7 +3,7 @@ import { MerkleTree } from "../src/merkle-tree-large";
 
 var assert = require('assert');
 
-async function main() {
+async function testDBMerkleTree() {
     const merkle_tree = new MerkleTree();
 
     await merkle_tree.loadSnapshot("0");
@@ -13,7 +13,7 @@ async function main() {
     await merkle_tree.setNode("0002", new Field(2));
     await merkle_tree.setNode("0003", new Field(3));
 
-    await merkle_tree.getNode("0001").then((node) => {
+    await merkle_tree.getNode("0001").then((node: any) => {
         assert.ok(node!.v.eq(new Field(1).v))
     });
 
@@ -21,21 +21,21 @@ async function main() {
     await merkle_tree.setNode("0001", new Field(19));
     await merkle_tree.endSnapshot();
 
-    await merkle_tree.lastestSnapshot().then((node) => {
+    await merkle_tree.lastestSnapshot().then((node: any) => {
         assert.equal(node, "2");
     });
 
-    await merkle_tree.getNode("0001").then((node) => {
+    await merkle_tree.getNode("0001").then((node: any) => {
         assert.ok(node!.v.eq(new Field(19).v))
     });
 
     await merkle_tree.loadSnapshot("1");
-    await merkle_tree.getNode("0001").then((node) => {
+    await merkle_tree.getNode("0001").then((node: any) => {
         assert.ok(node!.v.eq(new Field(1).v))
     });
 
     await merkle_tree.loadSnapshot("0");
-    await merkle_tree.getNode("0001").then((node) => {
+    await merkle_tree.getNode("0001").then((node: any) => {
         assert.ok(node!.v.eq(MerkleTree.emptyNodeHash(4).v));
     });
 
@@ -48,11 +48,39 @@ async function main() {
     await merkle_tree.setNode("0001", new Field(4));
     await merkle_tree.endSnapshot();
     await merkle_tree.loadSnapshot("0");
-    await merkle_tree.getNode("0001").then((node) => {
+    await merkle_tree.getNode("0001").then((node: any) => {
         assert.ok(node!.v.eq(MerkleTree.emptyNodeHash(4).v));
     });
 
     await merkle_tree.closeDb();
+}
+
+async function testInMemoryMerkleTree() {
+    const merkle_tree = new MerkleTree(true);
+
+    await merkle_tree.getNode("0001").then((node: any) => {
+        assert.ok(node === undefined)
+    });
+
+    await merkle_tree.setNode("0001", new Field(1));
+    await merkle_tree.getNode("0001").then((node: any) => {
+        assert.ok(node!.v.eq(new Field(1).v))
+    });
+
+    // lastestSnapshot should always return string "0"
+    await merkle_tree.lastestSnapshot().then((node: any) => {
+        assert.equal(node, "0");
+    });
+
+    // should do nothing
+    await merkle_tree.loadSnapshot("0");
+    await merkle_tree.endSnapshot();
+    await merkle_tree.closeDb();
+}
+
+async function main() {
+    await testDBMerkleTree();
+    await testInMemoryMerkleTree();
 }
 
 main().then(() => console.log("done"))
