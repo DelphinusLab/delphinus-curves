454e519cd34f344ec4dda6465b69e9e98a517131
cuiweixie <cuiweixie@gmail.com>
2024-12-19 10:39:56 +0800

    feat: add update_exact

--- test/poseidon.test.ts
+++ test/poseidon.test.ts
@@ -2,7 +2,7 @@ import BN from "bn.js";
 import { Field } from "../src/field";
 import { Poseidon } from "../src/poseidon";
 import { config } from "./config-basic";
-
+import { config as PoseidonConfig } from "../src/config";
 
 let hasher = new Poseidon(config);
 hasher.update([new Field(1)]);
@@ -28,4 +28,16 @@ if(hash.v.toString(16, 64) == "2f4b60f564113a7f93ee3ea3f94ad27ee649a3ccadc11746d
   console.log("Test failed");
   throw new Error("Test failed");
 }
-  
+
+{
+  let input = Array(PoseidonConfig.rate).fill(new Field(1));
+  let hasher = new Poseidon(PoseidonConfig);
+  let hash = hasher.update_exact(input);
+  console.log(hash.v.toString(16, 64));
+  if(hash.v.toString(16, 64) == "2998d3b7b2d5a959f531c143de78a3df6084ae2e7aced894610d1c4447f55483"){
+    console.log("Test passed");
+  } else {
+    console.log("Test failed");
+    throw new Error("Test failed");
+  } 
+}
